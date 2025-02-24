5df0a7f86451b965833e3c4bb522d972bd799d9a
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-02-24 17:07:13 -0300

    Update 24 - 06 - 14 (33).c
    Adição do responsável pela sugestão do código em sala de aula

--- Junho/24 - 06 - 14 (33).c
+++ Junho/24 - 06 - 14 (33).c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// ideia original de Lucas Cabral Kauvano:
+
 void ordenacao(char S[], int n){ //passagem de vetor string para uma função qualquer: str[].
     int j;
     
