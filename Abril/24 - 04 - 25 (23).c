129def0568bdd04d003cea9e5341e9d8962d5207
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-02-24 16:28:17 -0300

    Update 24 - 04 - 25 (23).c

--- Abril/24 - 04 - 25 (23).c
+++ Abril/24 - 04 - 25 (23).c
@@ -1,15 +1,11 @@
-Obs: a nota da prova tem como critério, também, a qualidade do programa, de tal forma que deve ser considerado um bom projeto,
-ou seja, sintético, correto e direto.
-
 #include <stdio.h>
 #include <math.h>
 
 void calculo(float pr, float *volume){
     float pi = 3.14;
-
     *volume = (4* pi * pow(pr, 3) )/3;
-
 }
+
 int main(){
     float raio, volume;
 
