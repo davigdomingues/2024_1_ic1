6d1246aa18585d841c2d10ed45bf5fca631a34d3
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-02-24 17:08:37 -0300

    Update 24 - 05 - 02 (noções de ponteiros e reaplicação de exercícios).c
    Correções de gramática nos comentários das autorias

--- Maio/24 - 05 - 02 (noções de ponteiros e reaplicação de exercícios).c
+++ Maio/24 - 05 - 02 (noções de ponteiros e reaplicação de exercícios).c
@@ -71,7 +71,7 @@ return 0;
 }
 */
 
-/* ou usar a solução da taisa
+/* ou usar a solução de Taysa Guiral:
 void main(){
 
 char frase[1000];
@@ -136,7 +136,7 @@ int main(){
 return 0;
 }
 
-/* solução do LUCAS CABRAL
+/* solução de Lucas Cabral Kauvano
 int f (char a, char b){
     int x = a - b;
     if (a > b) return x - 1;
