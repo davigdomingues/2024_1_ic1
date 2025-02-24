0b4522ac4ce278854176ee3971ecd343c8b28b11
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-02-24 16:30:07 -0300

    Update 24 - 04 - 22 (21).c

--- Abril/24 - 04 - 22 (21).c
+++ Abril/24 - 04 - 22 (21).c
@@ -1,45 +1,40 @@
 // solução em caminho "desnecessário".......
 #include <stdio.h>
+
 int func_1();
 char letra[256];
 int numero[256];
 int count = 0;
 int a, b;
 
-int main()
-{
+int main() {
     func_1();
-    
-    return 0;
+return 0;
 }
 
 int func_1(){
-    
     printf("Digite dois caracteres: ");
     scanf("%c %c", &letra[0], &letra[1]);
-    
     
     for (int j = 1; letra[j]; j++){
         printf("Valor em ASCII: %d %d\n", letra[j - 1], letra[j]);
         scanf("%d %d", &a, &b);
-        
     }
     
-        for (int i = 0; ; i++){
-            if(a < b){
-                count++;
-            } else {
-                break;
-            }
-        
-        }
+    for (int i = 0; ; i++){
+        if (a < b)
+            count++;
+            
+         else 
+            break;
+    }
         
     printf("O numero de caracteres entre eles eh: %d", count);
     
-    return 0;
+return 0;
 }
 
-// forma alternativa (inclusive, a melhor, neste caso:
+// forma alternativa:
 
 /*int main(){
     char a, b;
@@ -54,7 +49,6 @@ int func_1(){
         printf("O numero de caracteres entre eles eh: %d", diferenca - 1);
 
     else
-        printf("Meu filho, DEU ERROOOOOOOO!\n- Fabber linux , 2024");
-
+        printf("ERROOOOOOOO!");
 }
 */
